369746d7e682858c57b0ce36090ada2d9d40aa01
François BOITEUX <francois.boiteux@gmail.com>
2015-11-13 02:22:36 +0100

    home: display menu by role

--- src/Hippocrate/ViewModel/HomeViewModel.cs
+++ src/Hippocrate/ViewModel/HomeViewModel.cs
@@ -23,11 +23,59 @@ namespace Hippocrate.ViewModel
             }
         }
 
+        private string _tbutton1;
+        public string TButton1
+        {
+            get { return _tbutton1; }
+            set
+            {
+                _tbutton1 = value;
+                RaisePropertyChanged("TButton1");
+            }
+        }
+
+        private string _dbutton1;
+        public string DButton1
+        {
+            get { return _dbutton1; }
+            set
+            {
+                _dbutton1 = value;
+                RaisePropertyChanged("DButton1");
+            }
+        }
+
+        private string _tbutton2;
+        public string TButton2
+        {
+            get { return _tbutton2; }
+            set
+            {
+                _tbutton2 = value;
+                RaisePropertyChanged("TButton2");
+            }
+        }
+
+        private string _dbutton2;
+        public string DButton2
+        {
+            get { return _dbutton2; }
+            set
+            {
+                _dbutton2 = value;
+                RaisePropertyChanged("DButton2");
+            }
+        }
+
         /// <summary>
         /// Initializes a new instance of the LoginViewModel class.
         /// </summary>
         public HomeViewModel()
         {
+            TButton1 = "";
+            TButton2 = "";
+            DButton1 = "";
+            DButton2 = "";
             WindowContent = new View.HomeView();
             WindowContent.DataContext = this;
         }
